9e6ccdf4aebf74201b9e028ddfc00add5f25b45d
alouest <alex.saury@gmail.com>
2020-11-22 22:50:13 +0100

    add animationTree

--- Hero/Hero.gd
+++ Hero/Hero.gd
@@ -18,6 +18,8 @@ var state = MOVE
 var velocity = Vector2.ZERO
 
 onready var animationPlayer = $AnimationPlayer
+onready var animationTree = $AnimationTree
+onready var animationState = animationTree.get("parameters/playback")
 
 func _ready():
 	screen_size = get_viewport_rect().size
@@ -43,11 +45,13 @@ func move_state(delta):
 	input_vector = input_vector.normalized()
 	
 	if input_vector != Vector2.ZERO:
+		animationTree.set("parameters/Idle/blend_position", input_vector)
+		animationTree.set("parameters/Run/blend_position", input_vector)
 		velocity = velocity.move_toward(input_vector * MAX_SPEED, delta * ACCELERATION)
-		animationPlayer.play("RunRight")
+		animationState.travel("Run")
 	else:
 		velocity = velocity.move_toward(Vector2.ZERO, delta * FRICTION)
-		animationPlayer.play("Idle")
+		animationState.travel("Idle")
 	
 	move()
 	
